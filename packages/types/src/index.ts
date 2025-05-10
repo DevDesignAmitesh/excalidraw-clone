1bc3dc66b82a959c61178aa21a2923b788ee297c
DevDesignAmitesh <amiteshsingh252@gmail.com>
2025-05-10 13:00:30 +0530

    make the types more robust

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,14 +1,25 @@
 import z from "zod";
 
 export const SignUpTypes = z.object({
-  name: z.string().min(3, "name it too short"),
-  email: z.string().min(4).email("this is not a valid email"),
-  password: z.string().min(5, "password is to short"),
+  name: z.string().min(3, "name it too short").max(20, "name is too long"),
+  email: z
+    .string()
+    .min(4)
+    .email("this is not a valid email")
+    .max(20, "email is too long"),
+  password: z
+    .string()
+    .min(5, "password is to short")
+    .max(20, "password is too long"),
 });
 
 export const SignInTypes = z.object({
   email: z.string().min(4).email("this is not a valid email"),
   password: z.string().min(5, "password is to short"),
+});
+
+export const CreateRoomTypes = z.object({
+  name: z.string().min(4, "name is too short").max(20, "name is too long"),
 });
 
 export const JWT_SECRET = process.env.JWT_SECRET;
